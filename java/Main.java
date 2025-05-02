fde0976e00132299f5c2de26d9d2c8f5567e4d77
borq87 <52918084+borq87@users.noreply.github.com>
2025-05-02 18:14:28 +0300

    insertion sort

---
+++ java/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		int[] i1 = new int[3];
+		
+		i1[0] = 1;
+		i1[1] = 2;
+		
+		System.out.println(i1[0]);
+		System.out.println(i1[1]);
+
+		System.out.println(i1[2]); // 0 		
+	}
+}
